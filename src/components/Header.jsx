7cd86b3e91398ac6b6bfe5cba68003e9a46f4d93
prastowoadi <sinaadi75@gmail.com>
2022-10-08 00:41:58 +0700

    Second Commit Styling Navbar Integration with Firebase Setup react context Use Google Authentication Navbar in mobile screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 
-import { MdShoppingBasket } from "react-icons/md";
+import { MdAdd, MdLogout, MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
+
+  const [{ user }, dispatch] = useStateValue();
+
+  const [isMenu, setIsMenu] = useState(false);
+
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response);
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
+  };
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
   };
   return (
-    <header className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to="/" className="flex item-center gap-2">
           <img src={Logo} alt="logo-img" className="w-8 object-cover" />
@@ -25,7 +52,12 @@ const Header = () => {
         </Link>
 
         <div className="flex item-center gap-8">
-          <ul className="flex items-center gap-8">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-8"
+          >
             <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out">
               Home
             </li>
@@ -38,25 +70,105 @@ const Header = () => {
             <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out">
               Service
             </li>
-          </ul>
+          </motion.ul>
           <div className="flex items-center justify-center relative">
             <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
-            <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            <div className="absolute -top-1 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={Avatar}
+              src={user ? user.photoURL : Avatar}
               alt="avatar0img"
               className="w-10 min-w-[40px] h-10 min-h-[40px] shadow-md shadow-gray-600 rounded-full p-1 cursor-pointer"
+              referrerPolicy="no-referrer"
               onClick={login}
             />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col top-12 right-0 absolute py-2"
+              >
+                {user && user.email === process.env.EMAIL && (
+                  <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out">
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out"
+                  onClick={logout}
+                >
+                  Logout <MdLogout />
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
-      <div className="flex md:hidden w-full h-full"></div>
+      <div className="flex items-center justify-between md:hidden w-full h-full">
+        <div className="flex items-center justify-center relative">
+          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+          <div className="absolute -top-1 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+        </div>
+        <Link to="/" className="flex item-center gap-2">
+          <img src={Logo} alt="logo-img" className="w-8 object-cover" />
+          <p className="text-headingColor text-xl font-bold"> City</p>
+        </Link>
+        <div className="relative">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : Avatar}
+            alt="avatar0img"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] shadow-md shadow-gray-600 rounded-full p-1 cursor-pointer"
+            referrerPolicy="no-referrer"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col top-12 right-0 absolute py-2"
+            >
+              {user && user.email === "prastowoadidev@gmail.com" && (
+                <Link to={"/createItem"}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer text-textColor hover:bg-slate-100 transition-all duration-100 ease-in-out">
+                    New Item <MdAdd />
+                  </p>
+                </Link>
+              )}
+              <ul className="flex flex-col">
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+                  Home
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+                  Menu
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+                  About Us
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+                  Service
+                </li>
+              </ul>
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center justify-center gap-3 cursor-pointer text-textColor bg-gray-200 hover:bg-gray-300 transition-all duration-100 ease-in-out"
+                onClick={logout}
+              >
+                Logout <MdLogout />
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
